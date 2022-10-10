109d6371f88679d52791739be4e51bb9cf1664f1
Darren Neimke <darren.neimke@gmail.com>
2022-10-11 06:24:15 +1030

    Added dependency and initial working unit test

---
+++ cicd-sandpit.tests/Compute/Average/ShouldComputeAverage.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+using static cicd_sandpit.tests.Test;
+
+namespace cicd_sandpit.tests.Compute.Average
+{
+    public class ShouldComputeAverage : ContainerTestBase
+    {
+        List<int>?_numbers;
+        double? _average;
+        double _expected = 4;
+        
+        void GivenAListOfNumbers()
+        {
+            _numbers = new List<int>()
+            {
+                2, 4, 6
+            };
+        }
+
+        void WhenTheAverageIsComputed()
+        {
+            _average = cicd_sandpit.Compute.Average(_numbers!);
+        }
+
+        void ThenTheAverageShouldBeCorrect()
+        {
+            _average.ShouldBe(_expected);
+        }
+    }
+}
---
+++ cicd-sandpit/Compute.cs
@@ -0,0 +1,10 @@
+﻿namespace cicd_sandpit
+{
+    public static class Compute
+    {
+        public static double Average(List<int> list)
+        {
+            return list.Average();  
+        }
+    }
+}
